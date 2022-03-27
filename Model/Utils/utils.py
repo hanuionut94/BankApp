b2e6da89453c027498ab8359118f7a298f30a2dc
Hanu Ionut <i_hanu@yahoo.com>
2022-03-27 13:51:45 +0300

    Delete Model/Utils directory

--- Model/Utils/utils.py
+++
@@ -1,6 +0,0 @@
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import create_engine
-
-Base = declarative_base
-
-engine = create_engine('mysql+pymysql://root@localhost:3306/BankApp')
